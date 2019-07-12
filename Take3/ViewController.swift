fc8042777b87e85e006ff33907e92b3b5ff68215
Abbey Malbon <abbeymalbon@Abbeys-MacBook-Pro.local>
2019-07-12 15:33:08 -0500

    StatusHere

--- Take3/ViewController.swift
+++ Take3/ViewController.swift
@@ -9,17 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let emojis = ["🐚":"conch"]
+    let custom_message = ["conch": ["Oh Magic Conch Shell, what do we need to do to get out of the Kelp Forest?"]]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-        @IBAction func showMessage(sender: UIButton) {
-      let alertController = UIAlertController(title: "title", message: "message", preferredStyle: UIAlertController.Style.alert)
-            
-            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-            
-            present(alertController, animated: true, completion: nil)
+    @IBAction func showMessage(sender: UIButton)
+    {
+      let selectedEmotion = sender.titleLabel?.text
+     
+        
+        let alertController = UIAlertController(title: "The Magic Conch", message: "Oh Magic Conch Shell, what do we need to do to get out of the Kelp Forest?", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+        let emojiMessage = custom_message[emojis[selectedEmotion!]!]?[0]
+        alertController.message = emojiMessage
     }
 
 
